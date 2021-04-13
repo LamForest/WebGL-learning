c7a7c35cce3ba0dc0dfabd9493915a2fbe2ab55b
G TL <martin_lang@163.com>
2021-04-13 14:22:54 +0800

    重构了代码，用VAO代替了VBO

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,6 +1,6 @@
 main();
 //  链接
-let cur_angle = 0.0
+let cur_angle = 0.0;
 function initShaderProgram(gl, vsSource, fsSource) {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
@@ -46,9 +46,10 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
-function initBuffers(gl) {
+function initBuffers(gl, programInfo) {
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
     const positions = [
         1.0, 1.0,
         -1.0, 1.0,
@@ -57,7 +58,36 @@ function initBuffers(gl) {
     ];
     gl.bufferData(gl.ARRAY_BUFFER,
         new Float32Array(positions),
-        gl.STATIC_DRAW);    
+        gl.STATIC_DRAW);   
+     
+    const VAO = gl.createVertexArray();
+    gl.bindVertexArray(VAO);
+    //链接vertex.glsl中的aVertexPosition
+    {
+        //shader中的attribute是vec4
+        //这里是2，少的部分会由GPU补齐
+        //Note that size​ does not have to exactly match the size used by the vertex shader. 
+        // If the vertex shader has fewer components than the attribute provides, then the extras are ignored. 
+        // If the vertex shader has more components than the array provides, 
+        // the extras are given values from the vector (0, 0, 0, 1) for the missing XYZW components.
+        const numComponents = 2;  // pull out 2 values per iteration
+        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+        const normalize = false;  // don't normalize
+        const stride = 0;         // how many bytes to get from one set of values to the next
+        // 0 = use type and numComponents above
+        const offset = 0;         // how many bytes inside the buffer to start from
+        // gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position); ?要吗
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        //顶点属性默认是禁用的， 需要启用
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexPosition);
+    }
 
 
 
@@ -73,10 +103,33 @@ function initBuffers(gl) {
         new Float32Array(colors),
         gl.STATIC_DRAW);
 
-    return {
-        position: positionBuffer,
-        color : colorBuffer
-    };
+    //顶点属性color链接
+    {
+        //shader中的attribute是vec4
+        const numComponents = 4;  // pull out 4 values per iteration
+        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+        const normalize = false;  // don't normalize
+        const stride = 0;         // how many bytes to get from one set of values to the next
+        // 0 = use type and numComponents above
+        const offset = 0;         // how many bytes inside the buffer to start from
+        // gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexColor,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexColor);
+    }
+
+    
+    gl.bindVertexArray(null)
+
+    return VAO;
+
+
 }
 
 function getMVPMatrix(gl, angle) {
@@ -115,82 +168,18 @@ function getMVPMatrix(gl, angle) {
 
 }
 
-function drawScene(gl, programInfo, buffers, angle) {
+function drawScene(gl, programInfo, VAO, angle) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
     // Clear the canvas before we start drawing on it.
-
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-
     let MVP = getMVPMatrix(gl, angle);
-
-
-    //顶点属性postion链接
-    {
-        //shader中的attribute是vec4
-        //这里是2，少的部分会由GPU补齐
-        //Note that size​ does not have to exactly match the size used by the vertex shader. 
-        // If the vertex shader has fewer components than the attribute provides, then the extras are ignored. 
-        // If the vertex shader has more components than the array provides, 
-        // the extras are given values from the vector (0, 0, 0, 1) for the missing XYZW components.
-        const numComponents = 2;  // pull out 2 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-        // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        //顶点属性默认是禁用的， 需要启用
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexPosition);
-    }
-
-    //顶点属性color链接
-    {
-        //shader中的attribute是vec4
-        //这里是2，少的部分会由GPU补齐
-        //Note that size​ does not have to exactly match the size used by the vertex shader. 
-        // If the vertex shader has fewer components than the attribute provides, then the extras are ignored. 
-        // If the vertex shader has more components than the array provides, 
-        // the extras are given values from the vector (0, 0, 0, 1) for the missing XYZW components.
-        const numComponents = 4;  // pull out 2 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-        // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexColor,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexColor);
-    }
-
+    
     // Tell WebGL to use our program when drawing
-
     gl.useProgram(programInfo.program);
 
     // 讲Uniform变量传送进GPU
@@ -198,16 +187,17 @@ function drawScene(gl, programInfo, buffers, angle) {
         programInfo.uniformLocations.projectionMatrix,
         false,
         MVP);
-    // gl.uniformMatrix4fv(
-    //     programInfo.uniformLocations.modelViewMatrix,
-    //     false,
-    //     modelViewMatrix);
+    
+    gl.bindVertexArray(VAO);
 
     {
         const offset = 0;
         const vertexCount = 4;
+        //从顶点buffer中第0个开始，画4个顶点
         gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
     }
+    
+    gl.bindVertexArray(null)
 }
 
 async function loadGLSLFiles() {
@@ -223,7 +213,7 @@ async function loadGLSLFiles() {
 function main() {
     //第一步获取context
     const canvas = document.querySelector('#glcanvas');
-    const gl = canvas.getContext('webgl');
+    const gl = canvas.getContext('webgl2');
 
     // If we don't have a GL context, give up now
     if (!gl) {
@@ -238,11 +228,10 @@ function main() {
     //promise执行成功会进入shaders.then
     //此时
     shaders.then((data) => {
-        // let vertexShader, fragmentShader;
-        // vertexShader = data["vertex"];
-        // fragmentShader = data["fragment"];
         const shaderProgram = initShaderProgram(gl, data["vertex"], data["fragment"]);
-        // const shaderProgram = initShaderProgram(gl, vertexShader, fragmentShader);
+
+        //一个匿名类的对象
+        //使用 const 声明的对象是可以被修改的，只是不能再对programInfo赋值了
         const programInfo = {
             program: shaderProgram,
             //opengl中的layout location
@@ -257,13 +246,9 @@ function main() {
             },
         };
 
-        const buffers = initBuffers(gl);
+        const VAO = initBuffers(gl, programInfo);
 
-        // Draw the scene
-        // drawScene(gl, programInfo, buffers);
-
-        var then = 0;
-
+        let then = 0;
         // Draw the scene repeatedly
         function render(now) {
           now *= 0.001;  // convert to seconds
@@ -272,7 +257,7 @@ function main() {
           console.log(cur_angle);
           then = now;
       
-          drawScene(gl, programInfo, buffers, cur_angle);
+          drawScene(gl, programInfo, VAO, cur_angle);
       
           requestAnimationFrame(render);
         }
